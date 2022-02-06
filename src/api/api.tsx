4c82543bea343f999ff1caed6a9d499402d609f2
gurkangul <gurkangul05@gmail.com>
2022-02-06 21:45:16 +0300

    upt

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,17 +4,13 @@ export default async function fetchData(api: IEnpoint, request?: IRequestData) {
   const customHeaders = new Headers({
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
+    "User-Agent": "*",
   });
   if (request?.token) customHeaders.append("token", `${request?.token}`);
 
   let response = await fetch(api.url, {
     method: api.method,
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
     headers: customHeaders,
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
     body: JSON.stringify(request?.data),
   });
 
